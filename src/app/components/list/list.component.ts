1577d1d4524dca69eba3a8f62c4e6b1d96d83720
lucasRodrigues0 <lucasrodrigues119816@gmail.com>
2023-07-16 18:54:19 -0300

    adicionando funcionalidade

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./list.component.less']
 })
 export class ListComponent {
-  public list: String[] = ['Tomar banho', 'jogar cs', 'treinar', 'falar com as gatinhas', 'dormir', 'vagabundear', 'curtir o mar'];
+
+  public list: String[] = [];
+
+  public tarefa: String = '';
+
+  public incluirTarefa() {
+    if(this.tarefa !== '') this.list.push(this.tarefa);
+    this.tarefa = '';
+  }
+
+  public limparLista() {
+    this.list = [];
+  }
+
+  public deletarTarefa(item: String) {
+    this.list.splice(this.list.indexOf(item), 1);
+  }
+
 }
+
+
